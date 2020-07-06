1331240c91269085fd5892bb290c500d23bee768
jahuanca <joan.huanca19@gmail.com>
2020-07-06 15:07:16 -0500

    prueba digital

--- node/src/migrations/20200514072304-create-trabajador.js
+++ node/src/migrations/20200514072304-create-trabajador.js
@@ -14,12 +14,6 @@ module.exports = {
           key: 'id'
         }
       },
-      id_persona:{type:Sequelize.INTEGER, allowNull: false,validate: {isInt: true, min: 0}
-        ,references:{
-          model:'Persona',
-          key:'id'
-        }
-      },
       observacion: {type: Sequelize.STRING(200), allowNull: true, validate: {notEmpty: true, len: [1,200]}},
       estado: {type: Sequelize.CHAR(1), allowNull: false, defaultValue: 'A',
         validate: {notEmpty: true, len: [1,1], isIn: [['A', 'I']], isAlpha: true}
--- node/src/models/trabajador.js
+++ node/src/models/trabajador.js
@@ -1,8 +1,21 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Trabajador = sequelize.define('Trabajador', {
-    id_persona:{type:DataTypes.INTEGER, allowNull: false,validate: {isInt: true, min: 0}},
     id_usuario: {type: DataTypes.INTEGER, allowNull: false, validate: {min: 1, isInt: true}},
+    dni: {type: DataTypes.CHAR(8), unique: true, allowNull: true, validate: {notEmpty: true, len: [8,8], isNumeric: true}},
+    nombre: {type: DataTypes.STRING(50), allowNull: false, validate: {notEmpty: true, len: [1,50]}},
+    apellido: {type: DataTypes.STRING(50), allowNull: false, validate: {notEmpty: true, len: [1,50]}},
+    direccion: {type: DataTypes.STRING(150), allowNull: true, validate: {notEmpty: true, len: [1,50]}},
+    latitud:{type: DataTypes.DOUBLE, allowNull:true, validate: {isNumeric: true}},
+    longitud:{type: DataTypes.DOUBLE, allowNull:true, validate: {isNumeric: true}},
+    celular: {type: DataTypes.CHAR(9), allowNull: true, validate: {len: [9,9], isNumeric: true}},
+    imagenes: {type: DataTypes.STRING(200), allowNull: true, validate: {notEmpty: true, len: [1,200]}},
+    validado: {type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false},
+    codigo_verificacion:{type: DataTypes.STRING(10), allowNull: true, 
+      get() {
+        return() => this.getDataValue('codigo_verificacion')
+      }
+    },
     observacion: {type: DataTypes.STRING(200), allowNull: true, validate: {notEmpty: true, len: [1,200]}},
     estado: {type: DataTypes.CHAR(1), allowNull: false, defaultValue: 'A',
       validate: {notEmpty: true, len: [1,1], isIn: [['A', 'I']], isAlpha: true}
